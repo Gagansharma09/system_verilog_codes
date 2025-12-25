f2950830b53cd3c6c5206e8b3649fa168096c66b
Gagansharma09 <163728342+Gagansharma09@users.noreply.github.com>
2025-12-25 18:28:16 +0530

    Implement testbench for queue operations
    Added a testbench module to demonstrate queue operations including push_front, push_back, insert, and pop_front.

---
+++ queues/src/queues_2_pop.sv
@@ -0,0 +1,24 @@
+module tb;
+  int arr[$];   // queue
+    int j;
+  initial begin
+    arr = {1,2,3};
+    $display("Initial arr = %0p", arr);
+
+    arr.push_front(7);
+    $display("After push_front(7) = %0p", arr);
+
+    arr.push_back(9);
+    $display("After push_back(9) = %0p", arr);
+
+    arr.insert(2, 10);
+    $display("After insert(2,10) = %0p", arr);
+    j=arr.pop_front();
+    $display("arr:%0p",arr);
+    $display("values of j:%0d",j);
+
+    $finish;
+  end
+endmodule
+
+
