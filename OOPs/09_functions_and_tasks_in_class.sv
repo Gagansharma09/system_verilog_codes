b24fe0dae34d28f6e32c6b2e9b4f33335b527765
Gagansharma09 <163728342+Gagansharma09@users.noreply.github.com>
2025-12-26 06:23:04 +0530

    Update 09_functions_and_tasks_in_class.sv

--- OOPs/09_functions_and_tasks_in_class.sv
+++ OOPs/09_functions_and_tasks_in_class.sv
@@ -1,1 +1,12 @@
-
+module tb;
+  function bit [4:0] add(input bit [3:0]ain,bin);
+     return ain+bin;
+  endfunction
+  bit [4:0] res=0;
+  bit [3:0] ain=4'b0100;
+  bit [3:0] bin=4'b0010;
+  initial begin
+    res=add(ain,bin);
+    $display("values of addition:%0d",res);
+  end 
+endmodule 
